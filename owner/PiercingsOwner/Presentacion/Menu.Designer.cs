4679497903667198bc8c914f2c616f212cc8617f
Gabbo0709 <gabbo0709@gmail.com>
2025-08-04 21:54:59 -0600

    Project rebase

---
+++ owner/PiercingsOwner/Presentacion/Menu.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PiercingsOwner.Presentacion
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tablaProductos = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)tablaProductos).BeginInit();
+            SuspendLayout();
+            // 
+            // tablaProductos
+            // 
+            tablaProductos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tablaProductos.Location = new Point(12, 12);
+            tablaProductos.Name = "tablaProductos";
+            tablaProductos.Size = new Size(776, 305);
+            tablaProductos.TabIndex = 0;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tablaProductos);
+            Name = "Menu";
+            Text = "Menu";
+            ((System.ComponentModel.ISupportInitialize)tablaProductos).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView tablaProductos;
+    }
+}
